f490dc52c4ae490f3627b442758d8020aacdb343
Tony Bradley <tonybradley@Tonys-Mac-mini.fios-router.home>
2019-05-18 10:45:06 -0400

    update example

--- tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="tiles" tilewidth="16" tileheight="16" spacing="1" tilecount="162" columns="18">
- <image source="tiles.png" trans="ff00ff" width="307" height="163"/>
-</tileset>
